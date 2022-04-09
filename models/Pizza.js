ea0ad736bfa1d35e22d833843bcd36386eaeffd8
Kelly Lee <amorfati38@gmail.com>
2022-04-08 21:23:55 -0600

    added validation

--- models/Pizza.js
+++ models/Pizza.js
@@ -4,10 +4,14 @@ const dateFormat = require('../utils/dateFormat');
 const PizzaSchema = new Schema(
   {
     pizzaName: {
-      type: String
+      type: String,
+      required: 'You need to provide a pizza name!',
+      trim: true
     },
     createdBy: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     createdAt: {
       type: Date,
@@ -15,7 +19,9 @@ const PizzaSchema = new Schema(
       get: (createdAtVal) => dateFormat(createdAtVal)
     },
     size: {
-      type: String,
+      type: String,  
+      required: true,
+      enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
       default: 'Large'
     },
     toppings: [],
